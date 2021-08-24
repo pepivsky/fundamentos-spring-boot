18abdaaa29abad65b9ec813db3cceacc1b0ef83b
pepivsky <ironglas68@gmail.com>
2021-08-24 18:24:30 -0500

    first commit

---
+++ src/main/java/com/pepivsky/fundamentosspring/FundamentosspringApplication.java
@@ -0,0 +1,13 @@
+package com.pepivsky.fundamentosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentosspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pepivsky/fundamentosspring/FundamentosspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pepivsky.fundamentosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
