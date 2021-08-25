c866a0c2b28f951889963f41e2a836c8f035594d
pepivsky <ironglas68@gmail.com>
2021-08-25 01:02:13 -0500

    creacion de segunda dependencia propia

---
+++ src/main/java/com/pepivsky/fundamentosspring/bean/MyBean2Implement.java
@@ -0,0 +1,8 @@
+package com.pepivsky.fundamentosspring.bean;
+
+public class MyBean2Implement implements MyBean{
+    @Override
+    public void print() {
+        System.out.println("Hola desde mi implementacion 2 propia del bean 2");
+    }
+}
--- src/main/java/com/pepivsky/fundamentosspring/configuration/MyConfigurationBean.java
+++ src/main/java/com/pepivsky/fundamentosspring/configuration/MyConfigurationBean.java
@@ -1,6 +1,7 @@
 package com.pepivsky.fundamentosspring.configuration;
 
 import com.pepivsky.fundamentosspring.bean.MyBean;
+import com.pepivsky.fundamentosspring.bean.MyBean2Implement;
 import com.pepivsky.fundamentosspring.bean.MyBeanImplement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,6 @@ public class MyConfigurationBean {
 
     @Bean // implementacion del bean
     public MyBean beanOperation() {
-        return new MyBeanImplement(); // devolviendo la implementacion
+        return new MyBean2Implement(); // devolviendo la implementacion
     }
 }
